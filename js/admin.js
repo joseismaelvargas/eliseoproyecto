f1bbee5067de6c2c952d9a2c0b096dbbc00474db
ismael <joseismaelvargas@users.noreply.github.com>
2024-08-14 16:04:22 -0300

    "page admin agregers"

--- js/admin.js
+++ js/admin.js
@@ -1,9 +1,39 @@
-class producto{
+class Productos{
     constructor(nombre,precio,info,foto){
-       this.id=crypto.randomUUID(),
-       this.nombre=nombre,
-       this.precio=precio,
-       this.info=info,
-       this.foto=foto
+         this.nombre=nombre,
+        this.id=crypto.randomUUID(),
+        this.precio=precio,
+        this.info=info,
+        this.foto=foto
+       
     }
 }
+
+
+
+
+
+// capturar
+const form=document.getElementById("form")
+const nombre=document.getElementById("nombre")
+const precio=document.getElementById("precio")
+const foto=document.getElementById("foto")
+const info=document.getElementById("info")
+// 
+const array=[]
+
+const funcioform=(e)=>{
+e.preventDefault()
+
+ const producto=new Productos(nombre.value,precio.value,info.value,foto.value)
+ array.push(producto)
+
+limpiarform()
+}
+const limpiarform=()=>{
+
+ form.reset()
+} 
+console.log(array)
+
+form.addEventListener("submit",funcioform)
